2b631a409210a21d30ec650cf16434cabbb0919b
Alexey Arseniev <alexey.arseniev@sap.com>
2019-11-27 12:17:37 +0100

    Updated

--- lunch-vision.js
+++ lunch-vision.js
@@ -16,6 +16,18 @@ const publishIterationName = "classifyModel";
     const results = await predictor.classifyImage(sampleProject.id, publishIterationName, testFile);
     if (results.predictions.length > 1 && (results.predictions[0].probability * 100.0).toFixed(2) > 80)
     {
+        const CognitiveServicesCredentials = require('ms-rest-azure').CognitiveServicesCredentials;
+        const EntitySearchAPIClient = require('azure-cognitiveservices-entitysearch');
 
+        let credentials = new CognitiveServicesCredentials('90e8c6684b7b4aefaf3102fe3d6dcca6');
+        let entitySearchApiClient = new EntitySearchAPIClient(credentials);
+
+        entitySearchApiClient.entitiesOperations.search(results.predictions[0].tagName).then((result) => {
+            console.log(result.queryContext);
+            console.log(result.entities.value);
+            console.log(result.entities.value[0].description);
+        }).catch((err) => {
+            throw err;
+        });
     }
 })()
